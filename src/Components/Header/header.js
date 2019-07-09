49b0f18bd7bce0cf0fbff6b2f61ae6856179c557
Ewelina <ewe.krysiak@gmail.com>
2019-07-09 11:42:46 +0200

    Update folderow

---
+++ src/Components/Header/header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header ()=>{
+    return (
+        <div>
+            <img src="" alt=""/>
+        </div>
+
+
+    )
+
+}
+
+const App ()=> {
+    retrun <Header/>
+}
+
+ReactDOM.render(<App/>, document.getElementById("app"));
